77640f9d53b8e3f918a232d3b75f090a907fd6a5
Lutfullo1 <ismoilovlutfullo1117@gmail.com>
2025-01-07 20:55:03 +0500

    test img slider

--- src/pages/home/components/banner/home-banner.jsx
+++ src/pages/home/components/banner/home-banner.jsx
@@ -4,6 +4,10 @@ import "./home-banner.scss";
 import vid1 from "../../../../assets/video/vid1.mp4";
 import vid2 from "../../../../assets/video/vid2.mp4";
 import vid3 from "../../../../assets/video/vid3.mp4";
+import slider2 from "../../../../assets/images/home/slider2.jpeg";
+// import poster1 from "../../../../assets/images/poster1.jpg"; // Add your poster images
+// import poster2 from "../../../../assets/images/poster2.jpg";
+// import poster3 from "../../../../assets/images/poster3.jpg";
 
 function SampleNextArrow(props) {
   const { className, style, onClick } = props;
@@ -36,10 +40,10 @@ function SamplePrevArrow(props) {
 const settings = {
   dots: false,
   infinite: true,
-  speed: 500, // Slide transition speed in ms
+  speed: 500,
   slidesToShow: 1,
   autoplay: true,
-  autoplaySpeed: 10000, // Time between slides in ms (10 seconds)
+  autoplaySpeed: 10000,
   slidesToScroll: 1,
   nextArrow: <SampleNextArrow />,
   prevArrow: <SamplePrevArrow />,
@@ -74,19 +78,37 @@ export const HomeBanner = () => {
     <div className="home__banner">
       <Slider className="slider" {...settings}>
         <div className="slider__box">
-          <video autoPlay muted loop className="slider__video">
+          <video
+            autoPlay
+            muted
+            loop
+            className="slider__video"
+            poster={slider2} // Poster image for this video
+          >
             <source src={vid1} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
         <div className="slider__box">
-          <video autoPlay muted loop className="slider__video">
+          <video
+            autoPlay
+            muted
+            loop
+            className="slider__video"
+            poster={slider2} // Poster image for this video
+          >
             <source src={vid2} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
         <div className="slider__box">
-          <video autoPlay muted loop className="slider__video">
+          <video
+            autoPlay
+            muted
+            loop
+            className="slider__video"
+            poster={slider2} // Poster image for this video
+          >
             <source src={vid3} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
